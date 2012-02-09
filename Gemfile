2fc5b1d6235b36cd5659518a27725dc2223be9e9
Francis Chong <francis@ignition.hk>
2012-02-09 19:59:12 +0800

    init commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0'
+gem 'bson_ext'
+gem 'mongoid'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+# gem 'jbuilder'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picket::Application.config.secret_token = '224bcc758869d019842c3ae27d4054a1af507c97eb3bb4daa019b6f6c3146fdc379db1487d5ac8035324ca4e34b8d0f0909bbda9804905104ae670b0f9ff6f33'
