2780e5711352735aecd0ade410a0c5f9e1892b0c
Francis Chong <francis@ignition.hk>
2012-02-10 09:48:56 +0800

    add receiver config

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address        => Settings.mailer.address,
+  :port           => Settings.mailer.port,
+  :authentication => :plain,
+  :user_name      => Settings.mailer.user_name,
+  :password       => Settings.mailer.password,
+  :domain         => Settings.mailer.domain
+}
+ActionMailer::Base.delivery_method = :smtp
