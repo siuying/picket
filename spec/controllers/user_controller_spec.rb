bc9a58499fbd7e78813e92aa5ec49f4a5d23bda7
Francis Chong <francis@ignition.hk>
2012-02-14 01:27:45 +0800

    add user controller spec

---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe UserController do
+  let(:bob) { Factory(:bob) }
+
+  context "User not logged in" do
+    describe "GET index" do
+      it "should be redirected to login page" do
+        get :index
+        response.should redirect_to(new_user_session_path)
+      end
+    end
+  end
+  
+  context "User logged in" do
+    before(:each) { sign_in bob }
+    describe "GET index" do
+      it "should open user profile page" do
+        get :index
+        response.should render_template("index")
+        assigns(:user).should == bob 
+      end
+    end
+
+    describe "POST update" do
+      it "should update user profile" do
+        user_id = bob.id
+        email = "a@example.com"
+
+        post :update, :user => {:email => email, :password => "", :password_confirmation => ""}
+        response.should redirect_to(profile_path)
+        User.find(user_id).email.should == email
+      end
+    end
+  end
+end
